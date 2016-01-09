38c96ea2cfcb2cbe7d85074d7ef43087297561e5
Vasiliy Faronov <vfaronov@gmail.com>
2016-01-09 19:36:33 +0300

    More specific citations for transfer codings

--- httpolice/known/transfer_coding.py
+++ httpolice/known/transfer_coding.py
@@ -5,11 +5,17 @@ from httpolice.known.base import KnownDict
 
 
 known = KnownDict([
- {'_': TransferCoding(u'chunked'), '_citations': [RFC(7230)]},
- {'_': TransferCoding(u'compress'), '_citations': [RFC(7230)]},
- {'_': TransferCoding(u'deflate'), '_citations': [RFC(7230)]},
- {'_': TransferCoding(u'gzip'), '_citations': [RFC(7230)]},
+ {'_': TransferCoding(u'chunked'),
+  '_citations': [RFC(7230, section=(4, 1))]},
+ {'_': TransferCoding(u'compress'),
+  '_citations': [RFC(7230, section=(4, 2, 1))]},
+ {'_': TransferCoding(u'deflate'),
+  '_citations': [RFC(7230, section=(4, 2, 2))]},
+ {'_': TransferCoding(u'gzip'),
+  '_citations': [RFC(7230, section=(4, 2, 3))]},
  {'_': TransferCoding(u'identity'), '_citations': [RFC(2616)]},
- {'_': TransferCoding(u'x-compress'), '_citations': [RFC(7230)]},
- {'_': TransferCoding(u'x-gzip'), '_citations': [RFC(7230)]}
+ {'_': TransferCoding(u'x-compress'),
+  '_citations': [RFC(7230, section=(4, 2, 1))]},
+ {'_': TransferCoding(u'x-gzip'),
+  '_citations': [RFC(7230, section=(4, 2, 2))]}
 ])
