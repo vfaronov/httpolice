586d79b8262f534930aff7ad7255b62cc7f0cd6d
Vasiliy Faronov <vfaronov@gmail.com>
2016-01-13 19:49:31 +0300

    Check for 411 requests when Content-Length is OK (notice 1097)

--- httpolice/response.py
+++ httpolice/response.py
@@ -183,3 +183,7 @@ def check_response_in_context(resp, req):
         elif not any(header.is_proactive_conneg(hdr.name)
                      for hdr in req.headers):
             resp.complain(1091)
+
+    if resp.status == st.length_required and \
+            req.headers.content_length.is_okay:
+        resp.complain(1097)
