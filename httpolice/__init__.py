e14ac88fef2d0ccb7112906d172ffeae461d6a63
Vasiliy Faronov <vfaronov@gmail.com>
2016-04-14 23:48:01 +0300

    Tweak httpolice/__init__.py

--- httpolice/__init__.py
+++ httpolice/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8; -*-
 
-__version__ = u'0.1.0'
+__version__ = '0.1.0.dev1'
 
 from httpolice.exchange import analyze_exchange
 from httpolice.structure import Exchange, Request, Response
+
+__all__ = ['Exchange', 'Request', 'Response', 'analyze_exchange']
