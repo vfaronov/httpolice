99df3c6634940190a97d032ce847e54aab5b0521
Vasiliy Faronov <vfaronov@gmail.com>
2016-05-16 18:28:31 +0300

    Fix parsing HTTPolice-Silence

--- httpolice/syntax/internal.py
+++ httpolice/syntax/internal.py
@@ -10,12 +10,12 @@ from httpolice.parse import (
     subst,
 )
 from httpolice.syntax.common import DIGIT
-from httpolice.syntax.rfc7230 import OWS, comma_list
+from httpolice.syntax.rfc7230 import RWS, comma_list
 
 
 notice_id = int << string1(DIGIT)                                       > pivot
 resp = subst(True) << literal('resp')                                   > pivot
-HTTPolice_Silence = comma_list(notice_id * maybe(skip(OWS) * resp))     > pivot
+HTTPolice_Silence = comma_list(notice_id * maybe(skip(RWS) * resp))     > pivot
 
 
 fill_names(globals(), citation=None)
