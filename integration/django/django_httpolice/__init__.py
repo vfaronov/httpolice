4bec17c92056993c5a50be3193c92fb2ecae16f4
Vasiliy Faronov <vfaronov@gmail.com>
2016-07-24 01:42:07 +0300

    Move integration packages to separate repositories
    https://github.com/vfaronov/django-httpolice
    https://github.com/vfaronov/mitmproxy-httpolice
    
    When I started HTTPolice, I didn't want to bother with this.
    But it seems like separating these components into their own repos
    is both right (because they are related only through the public API)
    and more convenient (for Travis and AllMyChanges.com, to name but two).

--- integration/django/django_httpolice/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8; -*-
-
-from django_httpolice.common import ProtocolError
-from django_httpolice.middleware import HTTPoliceMiddleware, backlog
-from django_httpolice.views import report_view
-
-__all__ = ['HTTPoliceMiddleware', 'ProtocolError', 'backlog', 'report_view']
--- integration/django/django_httpolice/__metadata__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8; -*-
-
-version = '0.2.0.dev1'
