e55579d99ef3b5886a21bb661180df9f992e4aec
Vasiliy Faronov <vfaronov@gmail.com>
2016-07-25 01:21:40 +0300

    tools/build_extra_doc.sh

---
+++ tools/build_extra_doc.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Prepare files to be uploaded to pythonhosted.org.
+# Because pythonhosted.org does not support versioning like Read the Docs does,
+# these files should only be uploaded when releasing a new "stable" version.
+
+set -e
+
+dir=extra_doc
+build_dir=$dir/_build
+rm -rf "$dir" "$build_dir"
+mkdir -p "$dir" "$build_dir"
+
+echo 'Generating notices list...'
+python tools/list_notices.py >"$build_dir/notices.html"
+
+echo 'Generating showcase report...'
+httpolice -i combined -o html test/combined_data/showcase.https \
+    >"$build_dir/showcase.html"
+
+echo 'Generating redirects to Read the Docs...'
+for name in api concepts django genindex har index install mitmproxy \
+            py-modindex quickstart reports search streams
+do
+    new_url="http://httpolice.readthedocs.io/en/stable/$name.html"
+    cat >"$build_dir/$name.html" <<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTTPolice documentation</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta http-equiv="refresh" content="0; url=$new_url">
+    </head>
+    <body>
+        <p>
+            The HTTPolice user manual has moved to
+            <a href="$new_url">Read the Docs</a>.
+        </p>
+        <p>
+            If you are not redirected automatically, click here:
+            <a href="$new_url">$new_url</a>
+        </p>
+    </body>
+</html>
+EOF
+done
+
+echo 'Zipping...'
+cd "$build_dir/"
+zip -r extra_doc.zip ./*
+cd - >/dev/null
+mv "$build_dir/extra_doc.zip" "$dir/"
+
+echo 'All done.'
