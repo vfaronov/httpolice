23f1c84663ed7591eea8cae3daa91b83ad225a41
Vasiliy Faronov <vfaronov@gmail.com>
2016-07-25 12:39:07 +0300

    CI: validate HTML

---
+++ tools/vnu.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Download and run a local copy of the `Nu Html Checker`__.
+# We do this (rather than simply upload to the online public instance)
+# in order to pin down the version of the checker
+# and thus ensure reproducibility.
+#
+# __ http://validator.github.io/validator/
+
+set -e
+
+RELEASES=https://github.com/validator/validator/releases
+VERSION=16.6.29
+
+JAVA=/usr/lib/jvm/java-8-oracle/bin/java
+test -e "$JAVA" || JAVA=java
+
+dir=$HOME/vnu
+
+action=$1
+shift
+
+case $action in
+    install)
+        rm -rf "$dir"
+        mkdir -p "$dir"
+        cd "$dir"
+        wget "$RELEASES/download/$VERSION/vnu.jar_$VERSION.zip" -O vnu.zip
+        unzip vnu.zip
+        ;;
+
+    validate)
+        "$JAVA" -jar "$dir/dist/vnu.jar" "$@"
+        ;;
+esac
