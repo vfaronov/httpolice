b2d9da529af551669cc40049c03c86874b50d873
Vasiliy Faronov <vfaronov@gmail.com>
2016-07-26 16:34:56 +0300

    Check CSS for warnings, not just errors

--- tools/css_validator.sh
+++ tools/css_validator.sh
@@ -3,6 +3,12 @@
 # There's no easy way to pin down the version of the CSS validator,
 # but let's hope it doesn't break very often.
 
-curl -si https://jigsaw.w3.org/css-validator/validator \
-    -F "file=@$1;type=text/css" -F output=soap12 | \
-    tee /dev/stderr | grep -q 'W3C-Validator-Status: Valid'
+curl --silent --show-error --fail \
+    https://jigsaw.w3.org/css-validator/validator \
+    --form "file=@$1;type=text/css" --form output=soap12 | \
+    # Show output for debug purposes
+    tee /dev/stderr | \
+    xmlstarlet sel --text -N m=http://www.w3.org/2005/07/css-validator \
+        -t -v //m:errorcount -v //m:warningcount - | \
+    # Exit with non-zero status if not both 0
+    grep -Eq '^00$'
