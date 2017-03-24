156afa7366ffa14397b44ea79713b739ba876b3d
Vasiliy Faronov <vfaronov@gmail.com>
2017-03-24 22:50:10 +0300

    Fix tools/afl/harness.py to not break tests

--- tools/afl/harness.py
+++ tools/afl/harness.py
@@ -5,11 +5,10 @@ import sys
 
 import httpolice.cli
 
-args = httpolice.cli.parse_args(sys.argv)
-
-import afl
-while afl.loop(100):
-    httpolice.cli.run_cli(args, sys.stdout, sys.stderr)
-
-# As suggestd by python-afl docs.
-os._exit(0)         # pylint: disable=protected-access
+if __name__ == '__main__':
+    args = httpolice.cli.parse_args(sys.argv)
+    import afl
+    while afl.loop(100):
+        httpolice.cli.run_cli(args, sys.stdout, sys.stderr)
+    # As suggested by python-afl docs.
+    os._exit(0)         # pylint: disable=protected-access
