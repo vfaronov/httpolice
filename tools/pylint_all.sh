8e70139933623e08ef50ebc18d776e6f0fe386ff
Vasiliy Faronov <vfaronov@gmail.com>
2017-07-14 14:48:32 +0300

    Forgot to remove the Python 3.6 guard from tools/pylint_all.sh

--- tools/pylint_all.sh
+++ tools/pylint_all.sh
@@ -4,11 +4,4 @@ set -e
 
 test -e setup.py || { echo 'must run from repo root' >&2; exit 1; }
 
-if [ "$TRAVIS_PYTHON_VERSION" = 3.6 ]; then
-    # Pylint on HTTPolice doesn't work under Python 3.6
-    # with the current versions of astroid (1.4.9 and master).
-    echo 'WARNING: skipping Pylint under Python 3.6' >&2
-    exit 0
-fi
-
 find . -name '*.py' | grep -Fv ./doc/conf.py | xargs pylint "$@"
